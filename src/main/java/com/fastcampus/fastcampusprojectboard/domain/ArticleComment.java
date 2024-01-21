6e86dc271846a925d38fee6d09043c133bd2f04d
Paul <cdaosldk@gmail.com>
2024-01-21 20:53:13 +0900

    #8_도메인_클래스_설계(게시글, 댓글)

---
+++ src/main/java/com/fastcampus/fastcampusprojectboard/domain/ArticleComment.java
@@ -0,0 +1,14 @@
+package com.fastcampus.fastcampusprojectboard.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+    private Long id;
+    private Article article;
+    private String contents;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}

8785a2e5a0ff85ea3a1b6aa6e9d7feb65d08dd09
Paul <cdaosldk@gmail.com>
2024-01-25 12:32:34 +0900

    #15 개발환경 설정 중 참고사항
    1. run 메서드 clode() 호출
    2. 데이터베이스 설정

--- src/main/java/com/fastcampus/fastcampusprojectboard/FastCampusProjectBoardApplication.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/FastCampusProjectBoardApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class FastCampusProjectBoardApplication {
 
 	public static void main(String[] args) {
+//		SpringApplication.run(FastCampusProjectBoardApplication.class, args).close();
 		SpringApplication.run(FastCampusProjectBoardApplication.class, args);
 	}
 

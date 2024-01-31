004401da290cb391fae735078c0dae139b66ce14
Paul <cdaosldk@gmail.com>
2024-01-31 12:39:53 +0900

    #19 구현

--- src/main/java/com/fastcampus/fastcampusprojectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/controller/ArticleController.java
@@ -27,7 +27,7 @@ public class ArticleController {
 
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, ModelMap map) {
-        map.addAttribute("article", null);
+        map.addAttribute("article", "article"); // TODO: 실제 데이터 넣기
         map.addAttribute("articleComments", List.of());
 
         return "articles/detail";
